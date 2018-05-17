e98d80a5eb51b1bc9ab427f90989e8e7b65da75a
Philipp Grafendorfer <philipp.grafendorfer@gmail.com>
2018-05-17 23:00:33 +0200

    graph db connection established

--- import.py
+++ import.py
@@ -8,7 +8,7 @@ import twitter data
 """
 
 import twitter
-from py2neo.database import Node, Relationship
+from py2neo.database import Node, Relationship, Graph, authenticate
 
 api = twitter.Api(consumer_key='Ievh0b33YTmj0ozWPle0nkGpv',
                   consumer_secret='f9JOjYhCWkLV0ZcmhsRS8gteKWebP8OPHyFxLCSAc3sYDy5gzR',
@@ -25,4 +25,16 @@ print([s.text for s in statuses])
 users = api.GetFriends()
 users[0]
 users[0].name
+
+authenticate("hobby-baeadknhlicigbkehaeddfal.dbs.graphenedb.com:24786", "data-importer", "b.Mc1xVNF39CKC.KlbOB8CvSSplBxsE")
+graph = Graph("bolt://hobby-baeadknhlicigbkehaeddfal.dbs.graphenedb.com:24786",
+              user="data-importer",
+              password="b.Mc1xVNF39CKC.KlbOB8CvSSplBxsE", bolt=True,
+              secure=True, http_port=24789, https_port=24780)
+
+
+graph.run("CREATE (n:Person {name:'Philipp'})")
+result = graph.run("MATCH (n:Person) RETURN n")
+for record in result:
+    print(record)
 
